e2c7e19d946c79a2667867512a0cde46f7c785b8
lingz <lz781@nyu.edu>
2013-08-11 08:14:12 +1200

    fixes

--- js/issuer.js
+++ js/issuer.js
@@ -6,7 +6,7 @@ jQuery(function() {
     var tax_id = button.attr("data-tax_id");
     if (button.hasClass("current-active")) {
       jQuery.post(root_url + "/?issuer", {issuer_active: tax_id});
-      var old_active = jQuery(".issuer-disabled");
+      var old_active = jQuery(".current-disabled");
       button.toggleClass("current-disabled current-active btn-success btn-primary").html("Active").attr("disabled", "disabled");
       if (old_active.length > 0) {
         old_active.toggleClass("current-disabled current-active btn-success btn-primary").html("Make Active").removeAttr("disabled");
